7722eeeb696a20beb797c1d73b247476889d2a72
Ivan T <PaMLLIbIK@yandex.ru>
2025-06-08 18:20:30 +0500

    Sprint 8: на проверку

--- test/FileBackedTaskManagerTest.java
+++ test/FileBackedTaskManagerTest.java
@@ -15,23 +15,19 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FileBackedTaskManagerTest extends TaskManagerTest {
+class FileBackedTaskManagerTest {
 
     @Test
     void addTask() throws IOException, ManagerSaveException {
         Task task = new Task("task1", "task1 description", Status.NEW,
-                LocalDateTime.of(2025, 1, 1, 1, 1, 1),
-                Duration.ofMinutes(10));
-        Epic epic = new Epic("e1", "e", Status.NEW,
-                LocalDateTime.of(2025, 1, 1, 1, 1, 1),
+                LocalDateTime.of(2025, 1, 1, 1, 1),
                 Duration.ofMinutes(10));
         Path path = Files.createTempFile(Path.of("test"), ".txt", "src");
         assertTrue(Files.exists(path));
         FileBackedTaskManager backedTaskManager = new FileBackedTaskManager(path);
         backedTaskManager.addTask(task);
-        backedTaskManager.addEpic(epic);
         List<String> list = Files.readAllLines(path);
-        assertEquals(3, list.size());
+        assertEquals(2, list.size());
         Files.delete(path);
     }
 }
